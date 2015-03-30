26cc68a349a034ef268815191cbfc8dc394da4bc
Dale Kocian <dale.kocian@gmail.com>
2015-03-30 06:03:26 -0500

    update settings activity to not be deprecated

--- app/src/main/java/io/github/dkocian/vtd_xml_example/SettingsActivity.java
+++ app/src/main/java/io/github/dkocian/vtd_xml_example/SettingsActivity.java
@@ -1,49 +1,54 @@
 package io.github.dkocian.vtd_xml_example;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * This preference activity has in its manifest declaration an intent filter for
  * the ACTION_MANAGE_NETWORK_USAGE action. This activity provides a settings UI
  * for users to specify network settings to control data usage.
  */
-public class SettingsActivity extends PreferenceActivity
-        implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
-
+public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Loads the XML preferences file.
-        addPreferencesFromResource(R.xml.preferences);
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Loads the XML preferences file.
+            addPreferencesFromResource(R.xml.preferences);
+        }
 
-        // Registers a callback to be invoked whenever a user changes a preference.
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Registers a callback to be invoked whenever a user changes a preference.
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Unregisters the listener set in onResume().
+            // It's best practice to unregister listeners when your app isn't using them to cut down on
+            // unnecessary system overhead. You do this in onPause().
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
 
-        // Unregisters the listener set in onResume().
-        // It's best practice to unregister listeners when your app isn't using them to cut down on
-        // unnecessary system overhead. You do this in onPause().
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    // Fires when the user changes a preference.
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // Sets refreshDisplay to true so that when the user returns to the network_activity_ui
-        // activity, the display refreshes to reflect the new settings.
-        NetworkActivity.refreshDisplay = true;
+        // Fires when the user changes a preference.
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            // Sets refreshDisplay to true so that when the user returns to the network_activity_ui
+            // activity, the display refreshes to reflect the new settings.
+            NetworkActivity.refreshDisplay = true;
+        }
     }
 }
